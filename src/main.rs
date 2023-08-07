3978af252b415acd0aa8b84784230cbd9389dadf
jeffock <jeffock1656@gmail.com>
2023-08-07 11:33:39 -0400

    pt1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,58 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(
+            DefaultPlugins
+                .set(ImagePlugin::default_nearest())
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "bevy_game_template".into(),
+                        resolution: (640., 480.).into(),
+                        resizable: false,
+                        ..default()
+                    }),
+                    ..default()
+                })
+                .build(),
+        )
+        .add_systems(Startup, setup)
+        .add_systems(Update, character_movement)
+        .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+
+    let texture = asset_server.load("character.png");
+
+    commands.spawn(SpriteBundle {
+        sprite: Sprite {
+            custom_size: Some(Vec2::new(100., 100.)),
+            ..default()
+        },
+        texture,
+        ..default()
+    });
+}
+
+fn character_movement(
+    mut characters: Query<(&mut Transform, &Sprite)>,
+    input: Res<Input<KeyCode>>,
+    time: Res<Time>,
+) {
+    for (mut transform, _) in &mut characters {
+        if input.pressed(KeyCode::W) {
+            transform.translation.y += 100.0 * time.delta_seconds();
+        }
+        if input.pressed(KeyCode::S) {
+            transform.translation.y -= 100.0 * time.delta_seconds();
+        }
+        if input.pressed(KeyCode::D) {
+            transform.translation.x += 100.0 * time.delta_seconds();
+        }
+        if input.pressed(KeyCode::A) {
+            transform.translation.x -= 100.0 * time.delta_seconds();
+        }
+    }
 }
